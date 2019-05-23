37ef66886464ee42a071d20a527b5f6329902395
satheeshdragon <satheeshdragon.satheesh@gmail.com>
2019-05-23 07:56:49 +0530

    Updates

---
+++ precache-manifest.e3e3915929214f28c667838bdec959bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d416e2952db6e3a4f812df846452862",
+    "url": "/myreactsite/index.html"
+  },
+  {
+    "revision": "b39d03a248ed45f5cccc",
+    "url": "/myreactsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/myreactsite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b39d03a248ed45f5cccc",
+    "url": "/myreactsite/static/js/main.7d1d2cb0.chunk.js"
+  },
+  {
+    "revision": "cafdb896047135b22842",
+    "url": "/myreactsite/static/js/runtime~main.3af3de83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
